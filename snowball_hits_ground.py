d99987af073d33e5d4af1b58fa4281ef70b09bd7
br00cew4yne <nncnep@gmail.com>
2024-06-23 11:39:05 +0200

    feat: better parameters

--- snowball_hits_ground.py
+++ snowball_hits_ground.py
@@ -11,11 +11,19 @@ def main():
     gui = ti.GUI("Snowball hits ground", res=512, background_color=0x0E1018)
     mpm = MPM(
         gui=gui,
-        quality=3,
+        E=1.4e5,  # Young's modulus (1.4e5)
+        # E=4.8e4,  # Young's modulus (1.4e5)
+        nu=0.2,  # Poisson's ratio (0.2)
+        zeta=10,  # Hardening coefficient (10)
+        theta_c=2.5e-2,  # Critical compression (2.5e-2)
+        theta_s=4.5e-3,  # Critical stretch (7.5e-3)
+        rho_0=4e2,  # Initial density (4e2)
+        sticky=0.3,  # The lower, the stickier the border
+        quality=3,  # Use a larger value for higher-res simulations
         initial_gravity=[0, -9.8],
         initial_positions=np.array([[0.5, 0.5]], dtype=np.float32),
-        initial_velocities=np.array([[0, 0]], dtype=np.float32),
-        initial_radii=np.array([0.04], dtype=np.float32),
+        initial_velocities=np.array([[0, -5]], dtype=np.float32),
+        initial_radii=np.array([0.06], dtype=np.float32),
     )
     mpm.run()
 
--- snowball_hits_snowball.py
+++ snowball_hits_snowball.py
@@ -13,16 +13,16 @@ def main():
         gui=gui,
         E=1.4e5,  # Young's modulus (1.4e5)
         nu=0.2,  # Poisson's ratio (0.2)
-        zeta=10,  # Hardening coefficient (10)
-        theta_c=2.5e-2,  # Critical compression (2.5e-2)
-        theta_s=7.5e-3,  # Critical stretch (7.5e-3)
+        zeta=8,  # Hardening coefficient (10)
+        theta_c=1.5e-2,  # Critical compression (2.5e-2)
+        theta_s=5.5e-3,  # Critical stretch (7.5e-3)
         rho_0=4e2,  # Initial density (4e2)
         sticky=0.5,  # The lower, the stickier the border
-        quality=3,  # Use a larger value for higher-res simulations
+        quality=1,  # Use a larger value for higher-res simulations
         initial_gravity=[0, -9.8],
-        initial_positions=np.array([[0.05, 0.595], [0.93, 0.615]], dtype=np.float32),
-        initial_velocities=np.array([[5, 0], [-3, 0]], dtype=np.float32),
-        initial_radii=np.array([0.04, 0.05], dtype=np.float32),
+        initial_positions=np.array([[0.07, 0.595], [0.91, 0.615]], dtype=np.float32),
+        initial_velocities=np.array([[6, 0], [-3, 0]], dtype=np.float32),
+        initial_radii=np.array([0.06, 0.07], dtype=np.float32),
     )
     mpm.run()
 
