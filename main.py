0e56982c708cd633557827f9e75f557c981d7566
br00cew4yne <nncnep@gmail.com>
2024-06-24 10:36:47 +0200

    refactor: 'hideSettings', 'writeFrames'

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
+from argparse import ArgumentParser, RawTextHelpFormatter
 from src.Configuration import Configuration
 from src.MLS_MPM import Simulation
 import taichi as ti
 import numpy as np
-from argparse import ArgumentParser, RawTextHelpFormatter
 
 
-def snowball_positions(position=[[0, 0]], radii=[0.5], n_particles=1000):
-    n_snowballs = len(position)
+def snowball_positions(positions=[[0, 0]], radii=[0.5], n_particles=1000):
+    n_snowballs = len(positions)
     group_size = n_particles // n_snowballs
     p = np.zeros(shape=(n_particles, 2), dtype=np.float32)
     thetas = np.linspace(0, 2 * np.pi, group_size + 2, dtype=np.float32)[1:-1]
     for i in range(n_particles):
         j = i // group_size
         r = radii[j] * np.sqrt(np.random.rand())
-        p[i, 0] = (r * np.sin(thetas[i % group_size])) + position[j][0]
-        p[i, 1] = (r * np.cos(thetas[i % group_size])) + position[j][1]
+        p[i, 0] = (r * np.sin(thetas[i % group_size])) + positions[j][0]
+        p[i, 1] = (r * np.cos(thetas[i % group_size])) + positions[j][1]
     return p
 
 
-def snowball_velocities(velocity=[[0, 0]], n_particles=1000):
-    n_snowballs = len(velocity)
+def snowball_velocities(velocities=[[0, 0]], n_particles=1000):
+    n_snowballs = len(velocities)
     group_size = n_particles // n_snowballs
     v = np.zeros(shape=(n_particles, 2), dtype=np.float32)
     for i in range(n_particles):
         j = i // group_size
-        v[i, 0] = velocity[j][0]
-        v[i, 1] = velocity[j][1]
+        v[i, 0] = velocities[j][0]
+        v[i, 1] = velocities[j][1]
     return v
 
 
@@ -64,7 +64,7 @@ def main():
             theta_c=1.5e-2,  # Critical compression (2.5e-2)
             theta_s=5.5e-3,  # Critical stretch (7.5e-3)
             sticky=0.5,  # The lower, the stickier the border
-            position=snowball_positions([[0.07, 0.595], [0.91, 0.615]], radii=[0.04, 0.08], n_particles=n_particles),
+            position=snowball_positions([[0.07, 0.595], [0.91, 0.615]], radii=[0.04, 0.06], n_particles=n_particles),
             velocity=snowball_velocities([[6, 0], [-3, 0]], n_particles=n_particles),
         ),
         Configuration(
@@ -87,8 +87,8 @@ def main():
     paused_help = "Pause the simulation."
     write_help = "Write frames to disk."
     parser = ArgumentParser(prog="main.py", epilog=p_epilog, formatter_class=RawTextHelpFormatter)
-    parser.add_argument("--showSettings", const=True, default=False, nargs="?", help=settings_help)
-    parser.add_argument("--writeToDisk", const=True, default=False, nargs="?", help=write_help)
+    parser.add_argument("--hideSettings", const=True, default=False, nargs="?", help=settings_help)
+    parser.add_argument("--writeFrames", const=True, default=False, nargs="?", help=write_help)
     parser.add_argument("--configuration", default=0, nargs="?", help=configuration_help, type=int)
     parser.add_argument("--paused", const=True, default=False, nargs="?", help=paused_help)
     args = parser.parse_args()
@@ -99,8 +99,8 @@ def main():
         quality=quality,
         n_particles=n_particles,
         initial_gravity=[0, -9.8],
-        should_write_to_disk=args.writeToDisk,
-        should_show_settings=args.showSettings,
+        should_write_to_disk=args.writeFrames,
+        should_show_settings=(not args.hideSettings),
         configuration_id=args.configuration,
         configurations=configurations,
         is_paused=args.paused,
